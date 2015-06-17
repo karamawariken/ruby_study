1dbc55aee223fd6a9e3d9b639dcfe9eae1cb15f6
kosei.nishi <kosei.nishi@litalico.co.jp>
2015-06-17 22:32:57 +0900

    9章終わり　演習まだ

--- Gemfile
+++ Gemfile
@@ -33,6 +33,12 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
+#サンプルユーザを自動で作成する
+gem 'faker', '1.1.2'
+
+#ページネーション
+gem 'will_paginate', '3.0.4'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  #ユーザにサインインを要求するために以下メソッドを定義して呼び出す
+  #only ではアクションを限定している
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -18,9 +28,49 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+  def edit
+  end
+
+  def update
+    #update_attributes属性を後進するメソッド
+    if @user.update_attributes(user_params)
+      #更新に成功した場合に扱う
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
+    #ここにadminを入れないことにより任意のユーザが自分自身にアプリケーションの管理者権限を与えることを防止している
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    #Before actions
+    #flashでは、successとnoticeとerrorを設定できる
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    #管理者か確認
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
